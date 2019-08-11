0f8a719128b160b91534c3eecd2b43004e1ab317
KevinCurtisDev <kpcurtis@gmail.com>
2019-08-11 23:43:41 +0100

    Added dynamically created search form element

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+const searchContainer = document.querySelector('.search-container')
+const searchHtml = `<form action="#" method="get">
+                        <input type="search" id="search-input" class="search-input" placeholder="Search...">
+                        <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
+                    </form>`
+
+searchContainer.innerHTML = searchHtml;
