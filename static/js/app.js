ce316d962328271f69b23eac51815b737e91d62d
KevinCurtisDev <kpcurtis@gmail.com>
2019-08-12 00:47:01 +0100

    Created employee card template that returns info recieved from The Random user generator API

--- static/js/app.js
+++ static/js/app.js
@@ -7,6 +7,26 @@ const searchHtml = `<form action="#" method="get">
 searchContainer.innerHTML = searchHtml;
 
 
+const galleryImageContainer = document.querySelector('#gallery');
+
+const createEmployeeCard = (img, firstName, lastName, email, city, state) => {
+    let empCard = document.createElement('div');
+    empCard.setAttribute('class', 'card');
+
+    empCard.innerHTML =  `
+                    <div class="card-img-container">
+                        <img class="card-img" src=${img} alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
+                        <p class="card-text">${email}</p>
+                        <p class="card-text cap">${city}, ${state}</p>
+                    </div>
+                    `
+    galleryImageContainer.appendChild(empCard)
+}
+
+
 //Implemented the fetch API to retrieve random user data
 fetch('https://randomuser.me/api/?results=12').then(
     response => {
@@ -16,6 +36,16 @@ fetch('https://randomuser.me/api/?results=12').then(
             return;
           }
 
-          response.json().then(data => console.log(data));
+          response.json().then(data => {
+            for(let i = 0; i < 12; i++) {
+                console.log(data.results[i]);
+                createEmployeeCard(data.results[i].picture.thumbnail,
+                                   data.results[i].name.first,
+                                   data.results[i].name.last,
+                                   data.results[i].email,
+                                   data.results[i].location.city,
+                                   data.results[i].location.state)
+            }
+        });
     }
 )
